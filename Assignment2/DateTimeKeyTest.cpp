3681d330fdba0ebc123057429f49cae27e22c032
unknown <syfqdnl@gmail.com>
2025-11-22 03:39:31 +0800

    Commit changes

--- Assignment2/DateTimeKeyTest.cpp
+++ Assignment2/DateTimeKeyTest.cpp
@@ -23,37 +23,37 @@ void TestEquality()
 
 void TestOrdering()
 {
-    DateTimeKey k1(Date(, 1, 20231), Time(0, 0));
-    DateTimeKey k2(Date(2024, 1, 1), Time(0, 0));
+    DateTimeKey k1(Date(1, 1, 2023), Time(0, 0));
+    DateTimeKey k2(Date(1, 1, 2024), Time(0, 0));
 
     assert(k1 < k2);
     assert(k2 > k1);
 
     cout << "Year ordering OK\n";
 
-    DateTimeKey m1(Date(2024, 1, 1), Time(0, 0));
-    DateTimeKey m2(Date(2024, 2, 1), Time(0, 0));
+    DateTimeKey m1(Date(1, 1, 2024), Time(0, 0));
+    DateTimeKey m2(Date(1, 2, 2024), Time(0, 0));
 
     assert(m1 < m2);
 
     cout << "Month ordering OK\n";
 
-    DateTimeKey d1(Date(2024, 2, 10), Time(0, 0));
-    DateTimeKey d2(Date(2024, 2, 11), Time(0, 0));
+    DateTimeKey d1(Date(10, 2, 2024), Time(0, 0));
+    DateTimeKey d2(Date(11, 2, 2024), Time(0, 0));
 
     assert(d1 < d2);
 
     cout << "Day ordering OK\n";
 
-    DateTimeKey h1(Date(2024, 2, 10), Time(12, 0));
-    DateTimeKey h2(Date(2024, 2, 10), Time(13, 0));
+    DateTimeKey h1(Date(10, 2, 2024), Time(12, 0));
+    DateTimeKey h2(Date(10, 2, 2024), Time(13, 0));
 
     assert(h1 < h2);
 
     cout << "Hour ordering OK\n";
 
-    DateTimeKey min1(Date(2024, 2, 10), Time(12, 10));
-    DateTimeKey min2(Date(2024, 2, 10), Time(12, 20));
+    DateTimeKey min1(Date(10, 2, 2024), Time(12, 10));
+    DateTimeKey min2(Date(10, 2, 2024), Time(12, 20));
 
     assert(min1 < min2);
 
